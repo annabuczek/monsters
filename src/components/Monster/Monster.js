234bc97d3ae1b24941c735837dc8e03f7ef824a8
Anna Buczek <aania.buczek@gmail.com>
2020-07-25 20:25:45 +0200

    Add styles to the Monster component

--- src/components/Monster/Monster.js
+++ src/components/Monster/Monster.js
@@ -12,34 +12,36 @@ const Monster = ({
     <Element name={`monster-${index}`} className="monster">
       <div className="monster__wrapper">
         <h1 className="monster__name">{name}</h1>
-        <div className="monster__image-wrapper">
-          <img
-            className="monster__image"
-            src={images.big}
-            alt={`Monster ${name}`}
-          />
-        </div>
         <div className="monster__content">
-          <div className="monster__description">
-            {formatMonsterDescription(description).map(
-              (descriptionItem, index) => {
-                return (
-                  <p key={`description-item-${index}`}>
-                    {descriptionItem}
-                  </p>
-                );
-              },
-            )}
+          <div className="monster__image-wrapper">
+            <img
+              className="monster__image"
+              src={images.big}
+              alt={`Monster ${name}`}
+            />
           </div>
-          <div className="monster__statictics">
-            {Object.keys(statistics).map((key, index) => {
-              return (
-                <div key={`statistics-item-${index}`}>
-                  <span>{key}</span>
-                  <span>{statistics[key]}</span>
-                </div>
-              );
-            })}
+          <div className="monster__info">
+            <div className="monster__description">
+              {formatMonsterDescription(description).map(
+                (descriptionItem, index) => {
+                  return (
+                    <h3 key={`description-item-${index}`}>
+                      {descriptionItem}
+                    </h3>
+                  );
+                },
+              )}
+            </div>
+            <div className="monster__statictics">
+              {Object.keys(statistics).map((key, index) => {
+                return (
+                  <div key={`statistics-item-${index}`}>
+                    <span>{key}</span>
+                    <span>{statistics[key]}</span>
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
