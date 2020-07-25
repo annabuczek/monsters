4b69b569ecbd240572550bc1c358938b3f291c32
Anna Buczek <aania.buczek@gmail.com>
2020-07-25 18:21:07 +0200

    Add basic style to the Monster component

--- src/components/Monster/Monster.js
+++ src/components/Monster/Monster.js
@@ -6,7 +6,24 @@ import './Monster.scss';
 const Monster = ({ monster, index }) => {
   return (
     <Element name={`monster-${index}`} className="monster">
-      <h1 className="monster__name">{monster.name}</h1>
+      <div className="monster__wrapper">
+        <h1 className="monster__name">{monster.name}</h1>
+        <div className="monster__image-wrapper">
+          <img
+            className="monster__image"
+            src={monster.images.big}
+            alt={`Monster ${monster.name}`}
+          />
+        </div>
+        <div className="monster__content">
+          <p className="monster__description">
+            {monster.description}
+          </p>
+          <div className="monster__statictics">
+            {monster.statistics.power}
+          </div>
+        </div>
+      </div>
     </Element>
   );
 };
